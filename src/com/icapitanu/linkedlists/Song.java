5e3c1a12bef8530fe6f6358424f6a197d8dcbe28
Ionut Capitanu <ionut.capitanu@mambu.com>
2019-11-28 11:36:10 +0200

    Finished Album implementation & added Song factory method

--- src/com/icapitanu/linkedlists/Song.java
+++ src/com/icapitanu/linkedlists/Song.java
@@ -1,15 +1,15 @@
 package com.icapitanu.linkedlists;
 
-public class Song {
+class Song {
     private String songTitle;
     private int duration;
 
-    Song(String songTitle, int duration) {
+    public Song(String songTitle, int duration) {
         this.songTitle = songTitle;
         this.duration = duration;
     }
 
-    String getSongTitle() {
-        return songTitle;
+    private static Song createSong(String songTitle, int duration){
+        return new Song(songTitle,duration);
     }
 }
